a0bea9a0d30e6fe84b87791e1f2b8fd3c613cc8e
Michael Kwan <mk_zero16@hotmail.com>
2013-11-26 13:34:55 -0800

    Added files

---
+++ Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package p1;
+
+/**
+ * @author a00887509
+ *
+ */
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hello, world");
+
+    }
+
+}
